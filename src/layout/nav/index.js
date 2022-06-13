e091b8fa5049e93d3a711b74583c319747d241ec
yukairui <825294867@qq.com>
2022-06-13 16:00:11 +0800

    全局状态处理

--- src/layout/nav/index.js
+++ src/layout/nav/index.js
@@ -1,9 +1,24 @@
 import emitter from '@/utils/eventmanager.js'
 import './index.scss'
+import store from '@/stores/index'
+import { setToken } from '@/stores/token'
+import { useState } from 'react'
 function Nav () {
 
+  const [hadLogin, setHadLogin] = useState(false)
   const changeRoute = (path) => {
     emitter.emit('routeChange', path)
+  }
+
+  const signIn = () => {
+    // 可以在登录调接口的回调处理
+    store.dispatch(setToken('token'))
+    setHadLogin(true)
+  }
+
+  const signOut = () => {
+    store.dispatch(setToken(''))
+    setHadLogin(false)
   }
 
   return (
@@ -14,8 +29,8 @@ function Nav () {
         <div className="nav-item" onClick={ () => { changeRoute('/third') }}>third</div>
         <div className="nav-item" onClick={ () => { changeRoute('/fourth') } }>fourth</div>
       </div>
-      <div className="nav-right">
-        username
+      <div className="nav-right" onClick={ () => { !hadLogin ? signIn() : signOut() } }>
+        { hadLogin ? '已登录' : '未登录' }
       </div>
     </div>
   )
--- src/views/third/index.js
+++ src/views/third/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import store from '@/stores/index'
 class Third extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      token: ''
+    }
+  }
+  
+  componentDidMount () {
+    store.subscribe(() => {
+      this.setState({
+        token: store.getState().token.value
+      })
+    })
   }
   
   render () {
     return (
       <div>
-        这是第三个路由,展示类组件
+        这是第三个路由,展示类组件,顺便监听全局状态变化
+        <span>{ this.state.token}</span>
       </div>
     )
   }
