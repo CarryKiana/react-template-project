ac49531f392a6bcf2a3070cc0929407ff780a46f
yukairui <825294867@qq.com>
2022-06-13 16:47:38 +0800

    多环境打包配置

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+console.log(process.env.REACT_APP_BASE_ENV)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
